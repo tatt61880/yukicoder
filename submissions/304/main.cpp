618dbaba57221c9319f04abe4495ca961ad99a57
tatt61880 <tatt61880@gmail.com>
2020-04-22 22:10:00 +0900

    .

---
+++ submissions/304/main.cpp
@@ -0,0 +1,24 @@
+//{{{
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(...) repN((__VA_ARGS__,rep3,rep2,loop,~))(__VA_ARGS__)
+#define loop(n) rep2(_loop_,n)
+#define rep2(i,n) rep3(i,0,n)
+#define rep3(i,begin,end) for(int i=(int)(begin),i##_end=(int)(end);i<i##_end;++i)
+#define repN(a) repX a
+#define repX(a0,a1,a2,x,...) x
+const int MOD = 1e9+7;
+//}}}
+
+int main(){
+	cout << "000" << endl;
+	rep(i,1,1000){
+		string s;
+		cin >> s;
+		if(s[0]=='u') return 0;
+		printf("%03d", i);
+		cout << endl;
+	}
+	return 0;
+}
